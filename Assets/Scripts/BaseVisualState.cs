d07572017358623a64db40e8c3b78ce721b352b0
Retr0_Jaxon <james2060533824@gmail.com>
2026-01-30 22:24:10 +0800

    Reapply "State Machine Update"
    This reverts commit 4b6878c44831d8eb50a14ab2913ff0b1dc4f2d26.

---
+++ Assets/Scripts/BaseVisualState.cs
@@ -0,0 +1,13 @@
+﻿public abstract class BaseVisualState
+{
+    protected TileVisualStateManager manager; // 引用已更新
+
+    public BaseVisualState(TileVisualStateManager manager)
+    {
+        this.manager = manager;
+    }
+
+    public abstract void Enter();
+    public abstract void Update();
+    public abstract void Exit();
+}
