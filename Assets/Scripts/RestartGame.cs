ed33a75f897cc94ce058acfe3ff768617f5713ac
Gogol <2417001696@qq.com>
2026-02-01 02:40:55 +0800

    2button

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 必须引入这个命名空间
+
+public class RestartGame : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        // 获取当前活动场景的名字
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // 重新加载该场景
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    
+}
