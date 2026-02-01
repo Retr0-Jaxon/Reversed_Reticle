45ee68cad9b984ca3779f5543db0b14099e9faef
MacArthur <1840038994@qq.com>
2026-02-01 18:00:08 +0800

    适配webgl

---
+++ Assets/Scripts/FixAudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class FixAudioPlayer:MonoBehaviour
+{
+    [SerializeField]
+    private String audioName;
+
+    private void Awake()
+    {
+        var video = GetComponent<VideoPlayer>();
+        video.url= Application.streamingAssetsPath+"/"+audioName;
+        video.Play();
+        
+    }
+}
+
+
+
+
+
+
+
